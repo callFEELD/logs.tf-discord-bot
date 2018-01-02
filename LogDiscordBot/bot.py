72f0648d66299f278a0ede873ef891944eb0344f
callFEELD <admin@example.com>
2018-01-02 02:54:54 +0100

    Version 1.2
    Changes:
    - fixed commands that were unable to use
    - performance on '!logs' commands now shows also: assists,  ka/d, dmg,
    heals in percent, airshots and classes played
    - minor changes in styling of the output
    - added more tipsn. Its now easier to navigate through the bot
    
    - console output now shows errors if files are missing, wrong token
    added or no token added
    - more comments in the code
    - added a config file to easier change options

--- LogDiscordBot/bot.py
+++ LogDiscordBot/bot.py
@@ -1,23 +1,72 @@
-import discord
-import urllib.request, json
+# Log Bot
+# Description: A discord bot showing your recent logs, profile page and team matches. You can create teams,
+#              fill them with players and get recent matches of the teams. You can also search for other
+#              persons logs and logs.tf profile's.
+#
+# creator: callFEELD
+#          Website: https://callfeeld.jimdo.com/
+#          Steam:   http://steamcommunity.com/id/callFEELD/
+#          GitHub:  https://github.com/callFEELD
+# Thank you Matthew (GitHub: https://github.com/Matthew-The-Mighty-Mouse-Madge) for the support
+# last edit: 02.01.2017 (callFEELD)
 
 
-#import Classes
-from LogDiscordBot import LogDiscordBot
+# Importing third party libraries
+import discord                      # accessing the discord.py library
+import urllib.request, json         # used to access the token.json file
+import sys, time                    # used to handle errors
+
+# Importing own Classes
+from classes import LogDiscordBot
 ldbcommands = LogDiscordBot.LogBotCommands()
 ldbessentials = LogDiscordBot.LogBotEssentials()
 
+
+# Defining discord.py vars
 client = discord.Client()
 user = discord.User()
 
-tokenfile = open("token.json", "r").read()
-tokenjson = json.loads(tokenfile)
 
-#INSTALL
-TOKEN = tokenjson["token"]
-USERNAME = "logs.tf"
-PLAYING_STATUS = "type !logs help"
+# function to handle errors
+def error(error):
+    print('[i]  [status]        ERROR')
+    print("     [error type]        "+str(error))
+    if error == 1:
+        print("     [error msg]         Missing files to run the bot. Make sure you installed everything correct.")
 
+    elif error == 2:
+        print("     [error msg]         Couldn't read the token. Please insert the token into the token.json file.")
+
+    elif error == 3:
+        print("     [error msg]         Critical Error! Maybe wrong Token!")
+
+
+    time.sleep(5)
+    sys.exit()
+
+
+
+# Opening config files and other files
+try:
+    # Opening the token file and reading the token
+    tokenfile = open("token.json", "r").read()
+    tokenjson = json.loads(tokenfile)
+    TOKEN = tokenjson["token"]
+
+    #read the config
+    configfile = open("config.json", "r").read()
+    configjson = json.loads(configfile)
+    PLAYING_STATUS = configjson["playing_status"]
+except:
+    error(1)
+
+
+
+
+if TOKEN == "":
+    error(2)
+
+# Starting announcement
 print('starting LogDiscordBot (v.'+ldbessentials.version+')...')
 
 # sends message to the channel
@@ -28,6 +77,7 @@ async def sendMessage(message, messagetosend):
 
 @client.event
 async def on_ready():
+    # showing the UP and RUNNING screen after the bot is ready
     print('[i]  [status]        up and running')
     print("     [version]       " + ldbessentials.version)
     print("     [username]      " + client.user.name)
@@ -45,4 +95,9 @@ async def on_message(message):
         await sendMessage(message, messagetosend)
     messagetosend = None
 
-client.run(TOKEN)
+
+# connecting the bot to the discord servers
+try:
+    client.run(TOKEN)
+except:
+    error(3)
--- LogDiscordBot/install.py
+++ LogDiscordBot/install.py
@@ -1,3 +1,16 @@
+# IMPORTANT: STILL WORK IN PROGRESS
+# Log Bot
+# Description: A discord bot showing your recent logs, profile page and team matches. You can create teams,
+#              fill them with players and get recent matches of the teams. You can also search for other
+#              persons logs and logs.tf profile's.
+#
+# creator: callFEELD
+#          Website: https://callfeeld.jimdo.com/
+#          Steam:   http://steamcommunity.com/id/callFEELD/
+#          GitHub:  https://github.com/callFEELD
+# Thank you Matthew (GitHub: https://github.com/Matthew-The-Mighty-Mouse-Madge) for the support
+# last edit: 28.09.2017 (callFEELD)
+
 import os
 import time
 import json
